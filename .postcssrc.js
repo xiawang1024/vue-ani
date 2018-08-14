c9d178ce28c17d94856b33184b3d26be9142751d
wangxia/work <xiawang1024@icloud.com>
2018-08-14 15:13:15 +0800

    first commit

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,27 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
-}
+	plugins: {
+		'postcss-import': {},
+		'postcss-url': {},
+		autoprefixer: {},
+		'postcss-preset-env': {},
+		'postcss-aspect-ratio-mini': {},
+		'postcss-write-svg': {
+			utf8: false
+		},
+		'postcss-px-to-viewport': {
+			viewportWidth: 750,
+			viewportHeight: 1334,
+			unitPrecision: 3,
+			viewportUnit: 'vw',
+			selectorBlackList: [ '.ignore', '.hairlines' ],
+			minPixelValue: 1,
+			mediaQuery: false
+		},
+		'postcss-viewport-units': {},
+		cssnano: {
+			preset: 'advanced',
+			autoprefixer: false,
+			'postcss-zindex': false
+		}
+	}
+};
