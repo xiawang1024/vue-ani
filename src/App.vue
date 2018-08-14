f8409698904df95ad2cfda9e5c57e7fe36345efd
wangxia/work <xiawang1024@icloud.com>
2018-08-14 15:54:50 +0800

    feat:html完善

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Home msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style lang="scss" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import './common/scss/index.scss';
+import './common/scss/index.scss';
 
 /* 微信自定义分享 */
 import './weChat/index';
