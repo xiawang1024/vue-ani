4894869e2159668daaf6d64c979e5522176e8f66
wangxia/work <xiawang1024@icloud.com>
2018-08-16 10:23:02 +0800

    feat:配置

--- src/weChat/config.js
+++ src/weChat/config.js
@@ -1,5 +1,5 @@
 const appId = 'wx5f789dea59c6c2c5';
-const shareLink = 'http://www.hndt.com/nh5/tanabata/index.html';
+const shareLink = 'http://a.weixin.hndt.com/h5/tanabata/index.html';
 const shareTitle = '全世界退散，我要试手气';
 const shareDesc = 'Me的七夕图是哪张';
 const shareImg = require('./share.png');
