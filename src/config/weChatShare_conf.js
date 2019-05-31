c6cc5a771ab6a7d748f4a2dffa60c9885e0c2cb1
xiawang1024 <xiawang1024@gmail.com>
2019-05-31 14:47:19 +0800

    分享配置

--- src/config/weChatShare_conf.js
+++ src/config/weChatShare_conf.js
@@ -1,12 +1,12 @@
-const baseURL = 'http://www.hndt.com/nh5/lianghui/stick'
+const baseURL = "http://www.hndt.com/nh5/xinwen/20190531/html";
 /**
  * 打鱼晒网微信相关配置
  * appId 都市频道公众号
  */
-const appId = 'wxbf02a07137a4e2d3'
-const shareLink = `${baseURL}/index.html`
-const shareTitle = 'H5三部曲之“画”两会丨一根山药的海外“漂流”'
-const shareDesc = '河南广播电视台特别策划'
-const shareImg = `${baseURL}/icon-share.png`
+const appId = "wx5f789dea59c6c2c5";
+const shareLink = `${baseURL}/index.html`;
+const shareTitle = "高考便利贴";
+const shareDesc = "高考便利贴";
+const shareImg = `http://www.hndt.com/nh5/xinwen/20190531/imgs/icon-share.png`;
 
-export { appId, shareLink, shareTitle, shareDesc, shareImg }
+export { appId, shareLink, shareTitle, shareDesc, shareImg };
