b712fa4b76faea0b01da8e1b96a326eba4787f28
xiawang1024 <xiawang1024@gmail.com>
2019-08-23 11:27:28 +0800

    微信分享相关配置

--- src/config/weChatShare_conf.js
+++ src/config/weChatShare_conf.js
@@ -1,12 +1,20 @@
-const baseURL = "http://www.hndt.com/nh5/xinwen/20190531/html";
+/*
+ * @Description: weChat Config
+ * @Version: 0.0.1
+ * @Company: hNdt
+ * @Author: xiaWang1024
+ * @Date: 2019-03-04 10:45:55
+ * @LastEditTime: 2019-08-23 11:26:44
+ */
+const baseURL = "http://www.hndt.com/nh5/hndt/20190821/html";
 /**
- * 打鱼晒网微信相关配置
- * appId 都市频道公众号
+ * 微信相关配置
+ * appId 河南广播公众号
  */
 const appId = "wx5f789dea59c6c2c5";
 const shareLink = `${baseURL}/index.html`;
-const shareTitle = "2019“高考便利贴”， 考生请查收！";
-const shareDesc = "经历了汗水洗礼，才更懂得收获的喜悦！祝所有考生金榜题名！";
+const shareTitle = "来！给郑州地标翻牌子";
+const shareDesc = "提起郑州，你最先想到的是哪里？二七塔？火车站？大玉米？河南广播网梳理郑州的几大热门地标建筑，来为您中意的地标点赞打call吧~";
 const shareImg = `http://www.hndt.com/nh5/xinwen/20190531/imgs/icon-share.png`;
 
 export { appId, shareLink, shareTitle, shareDesc, shareImg };
