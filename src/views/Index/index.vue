a73f00c4355404e83c37ef236295be4f2470a7f3
xiawang1024 <xiawang1024@gmail.com>
2020-01-23 10:17:59 +0800

    动态增加slide

--- src/views/Index/index.vue
+++ src/views/Index/index.vue
@@ -2,45 +2,49 @@
   <div class="index">
     <swiper :options="swiperOption" ref="mySwiper" class="swiper-wrap">
       <swiper-slide class="swiper">
-        <Scenes :index="1"/>
+        <Scenes :index="1" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="2"/>
+        <ColleExam :index="2" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="3"/>
+        <ColleExam :index="3" />
+      </swiper-slide>
+      <!-- <swiper-slide class="swiper">
+        <ColleExam :index="4" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="4"/>
+        <ColleExam :index="5" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="5"/>
+        <ColleExam :index="6" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="6"/>
+        <Scenes :index="2" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <Scenes :index="2"/>
+        <ColleExam :index="7" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="7"/>
+        <ColleExam :index="8" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="8"/>
+        <ColleExam :index="9" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="9"/>
+        <Scenes :index="3" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <Scenes :index="3"/>
+        <ColleExam :index="10" />
       </swiper-slide>
       <swiper-slide class="swiper">
-        <ColleExam :index="10"/>
-      </swiper-slide>
-      <swiper-slide class="swiper">
-        <ColleExam :index="11"/>
-      </swiper-slide>
+        <ColleExam :index="11" />
+      </swiper-slide>-->
     </swiper>
+    <div style="display:none">
+      <ColleExam :index="11" ref="append" />
+    </div>
+    <button class="btn" @click="appendSlide">appendSlide</button>
   </div>
 </template>
 
@@ -83,6 +87,23 @@ export default {
         }
       }
     };
+  },
+  computed: {
+    swiperSlide() {
+      return this.$refs.mySwiper.swiper
+    }
+  },
+  methods: {
+    appendSlide() {
+      console.log(this.$refs.append)
+      console.log(this.$refs.append.$el.outerHTML)
+
+      // this.swiperSlide.appendSlide('<div class="swiper-slide">Slide 10</div>')
+      this.swiperSlide.appendSlide(`<div class='swiper swiper-slide swiper-slide-next'>${this.$refs.append.$el.outerHTML}</div>`)
+      setTimeout(() => {
+        this.swiperSlide.slideNext(750)
+      }, 300)
+    }
   }
 };
 </script>
@@ -95,6 +116,12 @@ export default {
   .swiper-wrap {
     width: 100%;
     height: 100%;
+  }
+  .btn {
+    position: fixed;
+    z-index: 1024;
+    left: 0;
+    top: 50px;
   }
 }
 </style>
